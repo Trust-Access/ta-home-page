f6bdd39d3d6e44a8678fdf3ae43187441554bf62
Raniro Coelho <ranirocoelho@gmail.com>
2025-08-11 18:32:49 -0300

    refactor: relocate story tests to component directories

--- components/atoms/mode-toggle/__tests__/ModeToggle.test.tsx
+++ components/atoms/mode-toggle/__tests__/ModeToggle.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import ModeToggle from "@/components/atoms/ui/mode-toggle";
+import ModeToggle from "@/components/atoms/mode-toggle";
 import { ThemeProvider } from "@/components/templates/theme-provider";
 import { vi } from "vitest";
 
